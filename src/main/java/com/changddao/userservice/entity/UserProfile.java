98245c2f07893430d63017675bc9187b6de846b9
changDDAO <younch8342@naver.com>
2025-07-18 15:41:45 +0900

    feat: UserProfile 관련 Entity 및 값타입 @Embeddable Address Entity 생성

---
+++ src/main/java/com/changddao/userservice/entity/UserProfile.java
@@ -0,0 +1,27 @@
+package com.changddao.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user_profiles")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class UserProfile {
+    @Id
+    private Long userId; //Auth-Service에서 생성 후, 전달됨
+
+    @Column(nullable = false, length = 40)
+    private String nickname;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length=20)
+    private String phoneNumber;
+
+    @Embedded
+    private Address address;
+}
