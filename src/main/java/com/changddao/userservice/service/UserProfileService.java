4874234b2c9b7ff1a68bce9c2f2069a567450634
changDDAO <younch8342@naver.com>
2025-07-18 16:34:07 +0900

    feat: UserProfileService 생성

---
+++ src/main/java/com/changddao/userservice/service/UserProfileService.java
@@ -0,0 +1,35 @@
+package com.changddao.userservice.service;
+
+import com.changddao.userservice.dto.CreateUserProfileRequest;
+import com.changddao.userservice.entity.Address;
+import com.changddao.userservice.entity.UserProfile;
+import com.changddao.userservice.repository.UserProfileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserProfileService {
+    private final UserProfileRepository userProfileRepository;
+
+    @Transactional
+    public void createProfile(CreateUserProfileRequest req) {
+        Address address = Address.builder()
+                .city(req.address().city())
+                .street(req.address().street())
+                .zipcode(req.address().zipcode())
+                .build();
+
+        UserProfile userProfile = UserProfile.builder()
+                .userId(req.userId())
+                .nickname(req.nickname())
+                .name(req.name())
+                .phoneNumber(req.phoneNumber())
+                .address(address)
+                .build();
+
+        userProfileRepository.save(userProfile);
+    }
+
+}
